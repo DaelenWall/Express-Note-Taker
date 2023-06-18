d62906cc5438f9ff82b22440556327a5151403d6
Daelen Wall <daelenwall@Vanessas-Air.fios-router.home>
2023-06-18 11:53:34 -0400

    rewriting all notes routes in apiRoutes (just requirements atm)

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,3 @@
-const path = require('path');
 const router = require('express').Router();
-
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
-})
-
-module.exports = router;
+const { v4: uuidv4 } = require('uuid')
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils.js');
