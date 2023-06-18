9ee85f703a7f1892d98abd405136ab17eaac067c
Daelen Wall <daelenwall@Vanessas-Air.fios-router.home>
2023-06-18 11:57:05 -0400

    readFromFile function for viewing already-made notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,7 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid')
 const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils.js');
+
+router.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
