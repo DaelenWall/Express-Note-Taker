3d2ae876a43ac0ea6c73a7556cb50642e077ae3d
Daelen Wall <daelenwall@Vanessas-Air.fios-router.home>
2023-06-18 11:51:30 -0400

    debugging html routing + switching to a indirect router (server.js-like)

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,8 @@
-const path = require('path');
-const router = require('express').Router();
+const express = require('express');
+const router = require('./apiRoutes');
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+const app = express();
 
-router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
-});
+app.use('/notes', router);
 
-module.exports = router;
+module.exports = app
