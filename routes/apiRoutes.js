cc8d64e08cf3494bbe36a31697a07e4a80cc78fb
Daelen Wall <daelenwall@Vanessas-Air.fios-router.home>
2023-06-18 12:27:12 -0400

    testing successful + (can use uuid for delete routes)

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,3 +5,24 @@ const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils
 router.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+
+    const { title, text, id } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added`);
+    } else {
+        console.error(err);
+    }
+});
+
+module.exports = router
